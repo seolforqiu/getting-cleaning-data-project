5c46a6349c0b933ae9d735ac87a6e5f3295ad2b5
seolforqiu <seolforqiu@gmail.com>
2015-02-22 19:23:04 -0500

    creat

---
+++ run_analysis.R
@@ -0,0 +1,61 @@
+library(reshape2)
+library(Hmisc)
+library(plyr)
+library(dplyr)
+
+## import data.frame of the train and test data sets
+setwd("./Google Drive/Course/Data Science/GetClnData")
+train <- read.table("./UCI HAR Dataset/train/X_train.txt")
+train_y <- read.table("./UCI HAR Dataset/train/y_train.txt")
+test <- read.table("./UCI HAR Dataset/test/X_test.txt")
+test_y <- read.table("./UCI HAR Dataset/test/y_test.txt")
+features <- read.table("./UCI HAR Dataset/features.txt")
+
+subtrain <- read.table("./UCI HAR Dataset/train/subject_train.txt")
+subtest <- read.table("./UCI HAR Dataset/test/subject_test.txt")
+
+## introduce the column names for the feature variables
+colnames(train) <- features$V2
+colnames(test) <- features$V2
+
+## row indices containing mean() or std() in features
+indices <- c(1:6,41:46, 81:86,121:126,161:166,201:202,214:215,227:228,240:241,253:254,266:271,
+             345:350, 424:429, 503:504, 516:517, 529:530, 542:543)
+selectedfeatures <- features[indices,]
+selectedfeatures <- as.vector(selectedfeatures$V2)
+trainselected <- train[,selectedfeatures]
+testselected <- test[,selectedfeatures]
+
+## bind the subject and activity info to the data.frame
+trainselected <- cbind(train_y, trainselected)
+testselected <- cbind(test_y, testselected)
+
+
+## use descriptive names for marking activities
+level <- c("WALKING","WALKING_UPSTAIRS", "WALKING_DOWNSTAIRS","SITTING","STANDING","LAYING")
+trainselected$V1 <- factor(trainselected$V1, 1:6, level)
+testselected$V1 <- factor(testselected$V1, 1:6, level)
+
+## change the column name of data.frame
+names(trainselected)[1]<-paste("activity")
+names(testselected)[1]<-paste("activity")
+
+## add subject info
+trainselected <- cbind(subtrain, trainselected)
+testselected <- cbind(subtest, testselected)
+
+names(trainselected)[1]<-paste("subject")
+names(testselected)[1]<-paste("subject")
+
+## create the 1st dataset
+trainselected$dataset <- 0
+testselected$dataset <- 1
+dataset <- rbind(trainselected,testselected)
+dataset$dataset <- factor(dataset$dataset, c(0,1), c("train","test"))
+
+## melt the data
+Molten <- melt(dataset, id = c("subject", "activity"), measure.vars=selectedfeatures)
+
+# create the second dataset to get the mean value for each activity and subject
+dataset2 <- dcast(Molten,subject+activity ~ variable, mean)
+write.table(dataset2, file="result.txt")
